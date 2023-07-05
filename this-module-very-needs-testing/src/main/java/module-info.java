6f6a78142b19df1471a88e60c4d9d430e2905ab1
dsfedotov <Dsfedotov@phoenixit.ru>
2023-07-05 11:13:22 +0300

    init

--- this-module-very-needs-testing/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.example.thismoduleveryneedstesting {
-  requires javafx.controls;
-  requires javafx.fxml;
-
-  opens com.example.thismoduleveryneedstesting to javafx.fxml;
-  exports com.example.thismoduleveryneedstesting;
-}
