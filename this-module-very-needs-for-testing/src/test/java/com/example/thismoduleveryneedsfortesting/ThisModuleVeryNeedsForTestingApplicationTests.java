023eb1c15519cabbd3be8d4c50fe4922819e7beb
dsfedotov <Dsfedotov@phoenixit.ru>
2023-10-03 14:34:05 +0300

    correct

--- this-module-very-needs-for-testing/src/test/java/com/example/thismoduleveryneedsfortesting/ThisModuleVeryNeedsForTestingApplicationTests.java
+++ this-module-very-needs-for-testing/src/test/java/com/example/thismoduleveryneedsfortesting/ThisModuleVeryNeedsForTestingApplicationTests.java
@@ -1,9 +1,5 @@
 package com.example.thismoduleveryneedsfortesting;
 
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
-
-import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
