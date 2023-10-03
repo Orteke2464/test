aefdb7b18090ad07e7f85dc028b8fed9b67bbf8a
dsfedotov <Dsfedotov@phoenixit.ru>
2023-10-03 14:33:34 +0300

    correct

--- this-module-very-needs-for-testing/src/test/java/com/example/thismoduleveryneedsfortesting/ThisModuleVeryNeedsForTestingApplicationTests.java
+++ this-module-very-needs-for-testing/src/test/java/com/example/thismoduleveryneedsfortesting/ThisModuleVeryNeedsForTestingApplicationTests.java
@@ -1,9 +1,14 @@
 package com.example.thismoduleveryneedsfortesting;
 
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT, properties = "server.port=8080")
 class ThisModuleVeryNeedsForTestingApplicationTests {
 
   @Test
